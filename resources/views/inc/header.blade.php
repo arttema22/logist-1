fc9cdbb3ace79a7c5aa80e19cf22f3201ccdb791
Artem Gusev <arttema@mail.ru>
2025-02-19 13:57:24 +0300

    refactor: edit main menu
    Delete items menu

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -32,64 +32,65 @@
                                 <hr class="dropdown-divider">
                             </li>
                             <li><a class="dropdown-item" href="{{ route('refilling.archive') }}">Архив</a></li>
-                            <li><a class="dropdown-item" href="{{ route('refilling.fuelsupplier') }}">Поставщики</a>
-                            </li>
-                        </ul>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">Выплаты</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ route('salary.create') }}">Новая</a></li>
-                            <li><a class="dropdown-item" href="{{ route('salary.list') }}">Список</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="{{ route('salary.archive') }}">Архив</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">Расчеты</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ route('profit.list') }}">Текущие</a></li>
-                            <li><a class="dropdown-item" href="{{ route('profit.archive') }}">Общая сверка</a></li>
-                        </ul>
-                    </li>
+                </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Выплаты</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="{{ route('salary.create') }}">Новая</a></li>
+                        <li><a class="dropdown-item" href="{{ route('salary.list') }}">Список</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="{{ route('salary.archive') }}">Архив</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Расчеты</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="{{ route('profit.list') }}">Текущие</a></li>
+                        <li><a class="dropdown-item" href="{{ route('profit.archive') }}">Общая сверка</a></li>
+                    </ul>
+                </li>
 
-                    @cannot('is-driver')
+                {{-- @cannot('is-driver')
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             Настройки
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            {{-- <li><a class="dropdown-item" href="{{ route('directory.type-trucks') }}">Типы
-                            грузовиков</a>
-                    </li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('directory.cargo') }}">Перевозимые
-                    грузы</a>
-                    </li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('directory.petrol-stations') }}">АЗС</a>
-                    </li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('directory.payers') }}">Плательщики</a>
-                    </li> --}}
-                    {{-- <li>
-                        <hr class="dropdown-divider">
-                    </li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('billing.distance') }}">Тарификация по
-                    расстоянию</a></li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('billing.route') }}">Тарификация по
-                    маршруту</a></li> --}}
-                    {{-- <li><a class="dropdown-item" href="{{ route('directory.services') }}">Услуги</a></li> --}}
-                    {{-- <li>
-                        <hr class="dropdown-divider">
-                    </li> --}}
-                    <li><a class="dropdown-item" href="{{ route('user.list') }}">Пользователи</a></li>
-                    <li><a class="dropdown-item" href="{{ route('user.roles') }}">Роли пользователей</a></li>
+                            <li><a class="dropdown-item" href="{{ route('directory.type-trucks') }}">Типы
+                грузовиков</a>
+                </li>
+                <li><a class="dropdown-item" href="{{ route('directory.cargo') }}">Перевозимые
+                        грузы</a>
+                </li>
+                <li><a class="dropdown-item" href="{{ route('directory.petrol-stations') }}">АЗС</a>
+                </li>
+                <li><a class="dropdown-item" href="{{ route('directory.payers') }}">Плательщики</a>
+                </li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="{{ route('billing.distance') }}">Тарификация по
+                        расстоянию</a>
+                </li>
+                <li><a class="dropdown-item" href="{{ route('billing.route') }}">Тарификация по
+                        маршруту</a></li>
+                <li><a class="dropdown-item" href="{{ route('directory.services') }}">Услуги</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="{{ route('user.list') }}">Пользователи</a></li>
+                <li><a class="dropdown-item" href="{{ route('user.roles') }}">Роли пользователей</a></li>
                 </ul>
                 </li>
-                @endcannot
+                @endcannot --}}
+
                 </ul>
                 <a class="btn btn-link" href="{{ route('user.edit', Auth::user()->id) }}">{{
                     Auth::user()->profile->FullName }}</a>
